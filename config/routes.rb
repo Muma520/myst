13e9c22d6463a8b8b277971889a203f04c0c3365
Muma520 <ndh.yw.0304.com>
2025-01-08 14:44:07 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :products
+  resource :session, only: [:new, :create, :destroy]
+  resource :registration, only: [:new, :create]
+  resources :passwords, param: :token
+  resources :products do
+    resources :subscribers, only: [ :create ]
+    resource :unsubscribe, only: [ :show ]
+  end
   root "products#index"
 end
